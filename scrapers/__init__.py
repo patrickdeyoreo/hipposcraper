a90cf5ce9b4feed448407c318181f73ea1d56385
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-06 09:46:32 -0800

    Reordered imports and added requests module

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,13 +1,14 @@
-import os
-import sys
-import re
-import string
 import json
+import os
+import re
+import requests
+import string
+import sys
 import urllib
 from bs4 import BeautifulSoup
 from base_parse import BaseParse
-from low_scraper import LowScraper
 from high_scraper import HighScraper
+from low_scraper import LowScraper
+from read_scraper import ReadScraper
 from sys_scraper import SysScraper
 from test_file_scraper import TestFileScraper
-from read_scraper import ReadScraper
